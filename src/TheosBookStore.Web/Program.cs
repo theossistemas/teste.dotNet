8449e3b82a485fa7c7f05c6dcb9d470b2264c728
Francisco Thiago <ftathiago@gmail.com>
2020-05-21 19:33:03 -0300

    Add Elastic-Kibana support

--- src/TheosBookStore.Web/Program.cs
+++ src/TheosBookStore.Web/Program.cs
@@ -1,11 +1,14 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
+
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Json;
+using Serilog.Sinks.Elasticsearch;
 
 namespace TheosBookStore.Web
 {
@@ -13,14 +16,70 @@ namespace TheosBookStore.Web
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            ConfigureLogging();
+            try
+            {
+                Log.Debug("Starting application");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal($"Failed to star the {Assembly.GetExecutingAssembly().GetName().Name}", ex);
+                throw;
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+          Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(configuration =>
+                {
+                    var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+                    configuration
+                        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                        .AddJsonFile(
+                            $"appsettings.{environment}.json",
+                            optional: true);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .UseSerilog();
+
+        private static void ConfigureLogging()
+        {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile(
+                    $"appsettings.{environment}.json",
+                    optional: true)
+                .Build();
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .Enrich.WithMachineName()
+                .WriteTo.Debug()
+                .WriteTo.Console()
+                .WriteTo.File(new JsonFormatter(), "./log.txt", LogEventLevel.Debug)
+                .WriteTo.Elasticsearch(ConfigureElasticSink(configuration, environment))
+                .Enrich.WithProperty("Environment", environment)
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+        }
+
+        private static ElasticsearchSinkOptions ConfigureElasticSink(IConfigurationRoot configuration, string environment)
+        {
+            var logIndex = Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-");
+            var uri = new Uri(configuration["ElasticConfiguration:Uri"]);
+            var options = new ElasticsearchSinkOptions(uri);
+            options.AutoRegisterTemplate = true;
+            options.AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv7;
+            options.IndexFormat = $"{logIndex}-{DateTime.UtcNow:yyyy-MM}";
+            options.EmitEventFailure = EmitEventFailureHandling.WriteToSelfLog |
+                                       EmitEventFailureHandling.WriteToFailureSink |
+                                       EmitEventFailureHandling.RaiseCallback;
+            Console.WriteLine(options.IndexFormat);
+            return options;
+        }
     }
 }
