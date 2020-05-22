f33c9420c70b2869d4a10215fb1ab86541a173b2
Francisco Thiago <ftathiago@gmail.com>
2020-05-21 22:34:07 -0300

    Create Entity Base class

---
+++ src/TheosBookStore.LibCommon/Entities/Entity.cs
@@ -0,0 +1,39 @@
+using FluentValidation.Results;
+
+namespace TheosBookStore.LibCommon.Entities
+{
+    public abstract class Entity
+    {
+        public ValidationResult ValidationResult { get => _validationResult; }
+        protected ValidationResult _validationResult = new ValidationResult();
+
+
+        public int Id { get; protected set; }
+
+        public void DefineId(int id)
+        {
+            this.Id = id;
+        }
+
+        public abstract bool IsValid();
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+
+            var compareTo = obj as Entity;
+
+            return Id.Equals(compareTo.Id);
+        }
+
+        public override int GetHashCode()
+        {
+            return (GetType().GetHashCode() * 42) + Id.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"{this.GetType().Name} Id[{Id.ToString()}]";
+        }
+    }
+}
