dd855a9ad2f8e3140ee4bc98b768f7ab9086f78f
Francisco Thiago <ftathiago@gmail.com>
2020-05-22 11:45:52 -0300

    Add tests do ISBN

--- __tests__/TheosBookStore.Stock.Domain.Tests/ValueObjects/ISBNTest.cs
+++ __tests__/TheosBookStore.Stock.Domain.Tests/ValueObjects/ISBNTest.cs
@@ -7,6 +7,10 @@ namespace TheosBookStore.Stock.Domain.Tests.ValueObjects
     public class ISBNTest
     {
         private const string VALID_ISBN = "978011000222";
+        private const string INVALID_ISBN_SMALL = "12345678901";
+        private const string INVALID_ISBN_BIGGER = "1234567890123";
+        private const string INVALID_ISBN_LETTERS = "12345678901a";
+
         [Fact]
         public void ShouldCreateISBN()
         {
@@ -14,6 +18,36 @@ namespace TheosBookStore.Stock.Domain.Tests.ValueObjects
 
             isbn.Should().NotBeNull();
             isbn.Value.Should().Be(VALID_ISBN);
+        }
+
+        [Fact]
+        public void ShouldBeInvalidWhenHaveLessThan12Digits()
+        {
+            var isbn = new ISBN(INVALID_ISBN_SMALL);
+
+            var validISBN = isbn.IsValid();
+
+            validISBN.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ShouldBeInvalidWhenBiggerThan12Digits()
+        {
+            var isbn = new ISBN(INVALID_ISBN_BIGGER);
+
+            var validISBN = isbn.IsValid();
+
+            validISBN.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ShouldHaveOnlyDigits()
+        {
+            var isbn = new ISBN(INVALID_ISBN_LETTERS);
+
+            var validISBN = isbn.IsValid();
+
+            validISBN.Should().BeFalse();
         }
     }
 }
