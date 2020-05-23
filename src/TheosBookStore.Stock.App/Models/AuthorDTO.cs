fc5a075cccabbf007466920db015866ebd6ab499
Francisco Thiago <ftathiago@gmail.com>
2020-05-22 21:05:04 -0300

    Create book DTO

---
+++ src/TheosBookStore.Stock.App/Models/AuthorDTO.cs
@@ -0,0 +1,8 @@
+namespace TheosBookStore.Stock.App.Models
+{
+    public class AuthorDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ src/TheosBookStore.Stock.App/Models/PublisherDTO.cs
@@ -0,0 +1,8 @@
+namespace TheosBookStore.Stock.App.Models
+{
+    public class PublisherDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
