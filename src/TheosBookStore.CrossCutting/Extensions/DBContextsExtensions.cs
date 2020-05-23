f1fac083f984d3bb4a38566e809b0a341d86461d
Francisco Thiago <ftathiago@gmail.com>
2020-05-23 10:15:22 -0300

    Create extension methods to database access config

---
+++ src/TheosBookStore.CrossCutting/Extensions/DBContextsExtensions.cs
@@ -0,0 +1,67 @@
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using TheosBookStore.CrossCutting.Models;
+using TheosBookStore.Stock.Infra.Context;
+
+
+namespace TheosBookStore.CrossCutting.Extensions
+{
+    public static class DBContextsExtensions
+    {
+        public static string BuildConnectionString(this IConfiguration configuration)
+        {
+            var appSettings = GetAppSettings(configuration);
+            var builder = new SqlConnectionStringBuilder();
+            builder.InitialCatalog = appSettings.DBName;
+            builder.DataSource = appSettings.DBHost;
+            builder.IntegratedSecurity = string.IsNullOrWhiteSpace(appSettings.DBUsuario) &&
+                string.IsNullOrWhiteSpace(appSettings.DBSenha);
+            builder.UserID = appSettings.DBUsuario;
+            builder.Password = appSettings.DBSenha;
+            var connectionString = builder.ConnectionString;
+            return connectionString;
+        }
+
+        private static AppSettings GetAppSettings(IConfiguration configuration)
+        {
+            var appSettings = new AppSettings();
+            appSettings.DBHost = configuration["DBHOST"] ?? configuration["DatabaseConfig:DatabaseHost"];
+            appSettings.DBUsuario = configuration["USERID"] ?? configuration["DatabaseConfig:UserID"];
+            appSettings.DBSenha = configuration["USERPASS"] ?? configuration["DatabaseConfig:UserPass"];
+            appSettings.DBName = configuration["UDBNAME"] ?? configuration["DatabaseConfig:DatabaseName"];
+            return appSettings;
+        }
+
+        public static IServiceCollection AddSqlServer(this IServiceCollection services,
+            string migrationAssembly, string connectionString)
+        {
+            services.AddDbContext<TheosBookStoreStockDB>(options =>
+                options
+                    .UseLazyLoadingProxies()
+                    .UseSqlServer(connectionString, m =>
+                        m.MigrationsAssembly(migrationAssembly))
+            );
+
+            return services;
+        }
+
+        public static IHost InitializeDataBase(this IHost host)
+        {
+            var serviceScopeFactory = host.Services;
+
+            using var scope = serviceScopeFactory.CreateScope();
+            var services = scope.ServiceProvider;
+
+            TheosBookStoreStockDB stockDB = services
+                .GetRequiredService<TheosBookStoreStockDB>();
+            stockDB.Database.Migrate();
+
+            return host;
+        }
+    }
+
+
+}
