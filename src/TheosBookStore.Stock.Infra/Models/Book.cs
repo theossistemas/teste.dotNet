ace5b66f79fcbadb9e0b85e9502d49204fbb7313
Francisco Thiago <ftathiago@gmail.com>
2020-05-23 10:12:33 -0300

    Update Book to follow code first patterns

--- src/TheosBookStore.Stock.Infra/Models/Book.cs
+++ src/TheosBookStore.Stock.Infra/Models/Book.cs
@@ -8,10 +8,11 @@ namespace TheosBookStore.Stock.Infra.Models
         public string Title { get; set; }
         public string ISBN { get; set; }
         public int PageCount { get; set; }
-        public int publisherid { get; set; }
+        public int PublisherId { get; set; }
         public int Year { get; set; }
         public int Edition { get; set; }
         public string City { get; set; }
+        public virtual Publisher Publisher { get; set; }
         public virtual ICollection<Author> Authors { get; set; }
     }
 }
