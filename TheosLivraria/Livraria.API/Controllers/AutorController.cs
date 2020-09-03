2a1eb4b2d42dac7e13ebb760e8e2493fa2d9dbbf
Rafael Arthur Rocha Miranda <43150739+rafael-miranda10@users.noreply.github.com>
2020-09-03 19:20:36 -0300

    endpoint para buscar autores

---
+++ TheosLivraria/Livraria.API/Controllers/AutorController.cs
@@ -0,0 +1,42 @@
+﻿using Livraria.Common.Model;
+using Livraria.Common.Utils;
+using Livraria.Domain.Dto;
+using Livraria.Domain.Interfaces.Repository;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Livraria.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutorController : BaseController
+    {
+        private readonly IAutorRepositorio _autorRepositorio;
+        public AutorController(
+            INotificationHandler<Notifications> notification,
+            IAutorRepositorio autorRepositorio) : base(notification)
+        {
+            _autorRepositorio = autorRepositorio;
+        }
+
+        [HttpGet]
+        [Authorize(Roles ="manager")]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var resultados = await _autorRepositorio.ListarAsync();
+                return Ok(resultados.Select(x => AutorDto.ConverterParaDto(x)));
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, Resources.MSG_Status500);
+            }
+        }
+
+    }
+}
