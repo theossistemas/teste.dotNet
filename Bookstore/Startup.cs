04984a0bec27d5340ac6075d8c764d916642ae96
Diogo Ferreira <diogo.fs@uol.com.br>
2021-01-21 16:09:03 -0400

    Instalação do Swagger.

--- Bookstore/Startup.cs
+++ Bookstore/Startup.cs
@@ -45,6 +45,8 @@ namespace Bookstore
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -68,6 +70,16 @@ namespace Bookstore
             {
                 app.UseSpaStaticFiles();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseRouting();
 
@@ -93,7 +105,7 @@ namespace Bookstore
                 {
                     spa.UseAngularCliServer(npmScript: "start");
                 }
-            });
+            });            
         }
     }
 }
