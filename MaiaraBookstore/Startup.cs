a5ae3e2f65fca206cd5e42f2408d65d7ae29d240
MaiFaria <mf.mai@hotmail.com>
2021-01-31 19:58:34 -0300

    Incluído permissão Cors

--- MaiaraBookstore/Startup.cs
+++ MaiaraBookstore/Startup.cs
@@ -37,11 +37,20 @@ namespace MaiaraBookstore
 
                 c.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
             });
+
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors("MyPolicy");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
