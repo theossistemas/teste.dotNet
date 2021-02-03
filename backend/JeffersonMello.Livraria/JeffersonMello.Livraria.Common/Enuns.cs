00677ef2c78546cbc1a4a872ac6e7b3aeebdc821
Jefferson Mello Olynyki <dev03@agrisolus.com.br>
2021-02-03 08:13:23 -0300

    Usuario

---
+++ backend/JeffersonMello.Livraria/JeffersonMello.Livraria.Common/Enuns.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace JeffersonMello.Livraria.Common
+{
+    public class Enuns
+    {
+        #region Public Enums
+
+        public enum NivelUsuario
+        {
+            [Description("Público")]
+            Publico = 0,
+
+            [Description("Administrador")]
+            Administrador = 1,
+        }
+
+        #endregion Public Enums
+    }
+}
---
+++ backend/JeffersonMello.Livraria/JeffersonMello.Livraria.Model/Cadastro/Pessoa/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using JeffersonMello.Livraria.Model.Abstract;
+
+namespace JeffersonMello.Livraria.Model.Cadastro.Pessoa
+{
+    public class Usuario : EntityBase
+    {
+        #region Public Properties
+
+        public string Nome { get; set; }
+
+        public string UsuarioAcesso { get; set; }
+
+        public string Senha { get; set; }
+
+        #endregion Public Properties
+
+    }
+}
